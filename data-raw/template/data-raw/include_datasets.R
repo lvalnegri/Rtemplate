c7f9b3a7d4131fd6ee131f0661bb7b2e405e8669
Luca Valnegri <l.valnegri@datamaps.co.uk>
2022-12-14 17:31:52 +0000

    minor details

--- data-raw/template/data-raw/include_datasets.R
+++ data-raw/template/data-raw/include_datasets.R
@@ -8,4 +8,4 @@ fn <- 'table_name'
 y <- fread('./data-raw/csv/filename.csv')
 assign(fn, y)
 save( list = fn, file = file.path('data', paste0(fn, '.rda')), version = 3, compress = 'gzip' )
-dmpkg.fun::dbm_do('db_name', 'w', fn, y)
+Rfuns::dd_dbm_do('db_name', 'w', fn, y)
